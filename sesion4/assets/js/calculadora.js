2777073bd9c0bdcfbfb2a678e25b7cdc31f03c9a
saulsneider7 <saulsneider@hotmail.com>
2023-07-10 20:18:25 -0500

    sesion 5

---
+++ sesion4/assets/js/calculadora.js
@@ -0,0 +1,29 @@
+function calcularSuma() {
+  //Capturamos el valor ingresado por el usuario
+  let valor1 = document.getElementById('valor1').value;
+  let valor2 = document.getElementById('valor2').value;
+
+  //Convertimos a numero
+  let valor1_transformado = parseFloat(valor1);
+  let valor2_transformado = parseFloat(valor2);
+
+  //Realizamos la operacion en un variable
+  let resultado = valor1_transformado + valor2_transformado;
+
+  //Agregamos el resultado al pagina web
+  let etiquetaResultado = document.createElement("p");
+  etiquetaResultado.id = "resultado";
+  etiquetaResultado.innerHTML = resultado;
+
+  document.getElementById('operandos').appendChild(etiquetaResultado);
+
+}
+function calcularResta() {
+  
+}
+function calcularProducto() {
+  
+}
+function calcularDivision() {
+  
+}
